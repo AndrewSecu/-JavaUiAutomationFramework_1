63cab574e1e1ae99fb483e029f6eb4d98b1d7778
Andrei Secu <andrei.s3cu@gmail.com>
2023-11-03 19:55:21 +0200

    Implemented the Logging System, Waiters, Reports

--- src/main/java/com/opencart/managers/DriverManager.java
+++ src/main/java/com/opencart/managers/DriverManager.java
@@ -1,5 +1,8 @@
 package com.opencart.managers;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,57 +10,68 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.time.Duration;
+
 public class DriverManager {
     private static String webDriverType = ConfigReaderManager.getPropertyValue("browserType");
     private static DriverManager instance;
     private WebDriver driver;
 
+    private static final Logger logger = LogManager.getLogger(DriverManager.class);
+
     private DriverManager() {
         switch (webDriverType.toUpperCase()) {
             case "CHROME":
-                ChromeOptions options =  new ChromeOptions();
+                ChromeOptions options = new ChromeOptions();
                 options.addArguments("ignore-certificate-errors");
                 options.addArguments("--start-maximized");
                 options.addArguments("--incognito");
                 driver = new ChromeDriver(options);
-                System.out.println("The Chrome Driver is initiated");
+                logger.log(Level.INFO, "The Chrome Driver is initiated");
                 break;
             case "FIREFOX":
                 driver = new FirefoxDriver();
-                System.out.println("The Firefox Driver is initiated");
+                logger.log(Level.INFO, "The Firefox Driver is initiated");
                 break;
             case "EDGE":
                 driver = new EdgeDriver();
-                System.out.println("The Edge Driver is initiated");
+                logger.log(Level.INFO, "The Edge Driver is initiated");
                 break;
             case "SAFARI":
                 driver = new SafariDriver();
-                System.out.println("The Safari Driver is initiated");
+                logger.log(Level.INFO, "The Safari Driver is initiated");
                 break;
             default:
-                System.out.println("There is not such a browser type " + webDriverType);
+                logger.log(Level.INFO, "There is not such a browser type " + webDriverType);
         }
+
+        int implicitWaitTime = Integer.parseInt(ConfigReaderManager.getPropertyValue("implicitWaiterValue"));
+        int pageLoadTime = Integer.parseInt(ConfigReaderManager.getPropertyValue("pageLoadTimeOut"));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitTime));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(pageLoadTime));
     }
 
-    public static DriverManager getInstance(){
-        if(instance == null){
+    public static DriverManager getInstance() {
+        if (instance == null) {
             instance = new DriverManager();
         }
         return instance;
     }
 
-    public WebDriver getDriver(){
+    public WebDriver getDriver() {
         return driver;
     }
 
-    public void tearDown(){
+    public void tearDown() {
         driver.close();
         driver.quit();
         instance = null;
         driver = null;
+        logger.log(Level.WARN, "The driver is null");
     }
 
-    public void deleteCookies(){
+    public void deleteCookies() {
         driver.manage().deleteAllCookies();
         try {
             Thread.sleep(1000);
